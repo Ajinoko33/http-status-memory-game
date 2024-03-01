32eb57c0756d3a5fdadce006f7462e147a8abe23
Ajinoko33 <2022kyopr03@gmail.com>
2024-03-01 21:43:36 +0900

    settings

--- http-status-memory-game/src/app/layout.tsx
+++ http-status-memory-game/src/app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Layout } from '@/components/layout';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "HTTP Status Memory Game",
-  description: "The memory game with http response status code",
+  title: 'HTTP Status Memory Game',
+  description: 'The memory game with http response status code',
 };
 
 export default function RootLayout({
@@ -16,9 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <AntdRegistry>{children}</AntdRegistry>
+        <AntdRegistry>
+          <Layout>{children}</Layout>
+        </AntdRegistry>
       </body>
     </html>
   );
---
+++ http-status-memory-game/src/components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Layout as AntDLayout } from 'antd';
+import { CSSProperties, ReactNode } from 'react';
+
+const { Content } = AntDLayout;
+
+const layoutStyle: CSSProperties = {
+  backgroundColor: 'white',
+  height: '100vh',
+  width: '100vw',
+};
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <AntDLayout style={layoutStyle}>
+      <Content>{children}</Content>
+    </AntDLayout>
+  );
+};
